569a9ee7331f3f1fe9efd6d75c7e5fecda1f5509
moth1995 <marcos-flores@live.com.ar>
2022-02-05 08:30:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PES-Editor-3
+ A tool to edit pes3/we7 option files
---
+++ save.py
@@ -0,0 +1,17 @@
+from editor.option_file import OptionFile
+
+# Put PES 5 Option File in the same folder as this script
+#of_file_location = "KONAMI-WIN32PES4OPT"
+of_file_location = r"D:\Juegos\KONAMI\Pro Evolution Soccer 4\save\folder1\KONAMI-WIN32PES4OPT"
+
+# Load/decrypt the option file
+print("Loading option file...")
+of = OptionFile(of_file_location)
+print("Option file loaded.")
+
+with open("temp.bin", "rb") as binary_file:
+    of.data = bytearray(binary_file.read())
+# Save/encrypt the option file
+print("Saving option file...")
+of.save_option_file('pes3test')
+print("Option file saved.")
